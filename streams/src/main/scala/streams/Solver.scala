89acded901717c29fec2f0d1d2b0ca9aac234856
Dani Acherkan <dtl.117@gmail.com>
2013-11-07 19:55:41 +0200

    paths

--- streams/src/main/scala/streams/Solver.scala
+++ streams/src/main/scala/streams/Solver.scala
@@ -10,7 +10,7 @@ trait Solver extends GameDef {
   /**
    * Returns `true` if the block `b` is at the final position
    */
-  def done(b: Block): Boolean = ???
+  def done(b: Block): Boolean = b.b1 == goal && b.b2 == goal
 
   /**
    * This function takes two arguments: the current block `b` and
@@ -28,15 +28,15 @@ trait Solver extends GameDef {
    * It should only return valid neighbors, i.e. block positions
    * that are inside the terrain.
    */
-  def neighborsWithHistory(b: Block, history: List[Move]): Stream[(Block, List[Move])] = ???
-
+  def neighborsWithHistory(b: Block, history: List[Move]): Stream[(Block, List[Move])] =
+    (b.legalNeighbors map (x => (x._1, x._2 :: history))).toStream
   /**
    * This function returns the list of neighbors without the block
    * positions that have already been explored. We will use it to
    * make sure that we don't explore circular paths.
    */
   def newNeighborsOnly(neighbors: Stream[(Block, List[Move])],
-                       explored: Set[Block]): Stream[(Block, List[Move])] = ???
+                       explored: Set[Block]): Stream[(Block, List[Move])] = neighbors filter (x => !(explored contains x._1))
 
   /**
    * The function `from` returns the stream of all possible paths
@@ -62,7 +62,24 @@ trait Solver extends GameDef {
    * construct the correctly sorted stream.
    */
   def from(initial: Stream[(Block, List[Move])],
-           explored: Set[Block]): Stream[(Block, List[Move])] = ???
+           explored: Set[Block]): Stream[(Block, List[Move])] = 
+           if (initial.isEmpty) Stream.empty
+           else {
+        	   val more = for {
+        		   path <- initial
+        		   next <- newNeighborsOnly(neighborsWithHistory(initial.head._1, initial.head._2), explored)
+        	   } yield next
+        	   paths #:: from(more, explored ++ (more map (_.endState)))
+           }
+
+           
+           
+           {for {
+             n <- newNeighborsOnly(neighborsWithHistory(initial.head._1, initial.head._2), explored)
+           } yield n #:: from() 
+           Stream()
+  }
+           
 
   /**
    * The stream of all paths that begin at the starting block.
@@ -85,3 +102,4 @@ trait Solver extends GameDef {
    */
   lazy val solution: List[Move] = ???
 }
+
--- streams/src/main/scala/streams/StringParserTerrain.scala
+++ streams/src/main/scala/streams/StringParserTerrain.scala
@@ -2,6 +2,7 @@ package streams
 
 import common._
 
+
 /**
  * This component implements a parser to define terrains from a
  * graphical ASCII representation.
@@ -52,7 +53,15 @@ trait StringParserTerrain extends GameDef {
    * a valid position (not a '-' character) inside the terrain described
    * by `levelVector`.
    */
-  def terrainFunction(levelVector: Vector[Vector[Char]]): Pos => Boolean = ???
+  def terrainFunction(levelVector: Vector[Vector[Char]]): Pos => Boolean = {
+    val tileChars = Set('o','S','T')
+    val tiles = for{
+      i <- 0 until levelVector.length
+      j <- 0 until levelVector(i).length
+      if tileChars contains levelVector(i)(j)
+    } yield Pos(i, j)
+    (x => tiles contains x)
+  }
 
   /**
    * This function should return the position of character `c` in the
@@ -62,7 +71,11 @@ trait StringParserTerrain extends GameDef {
    * Hint: you can use the functions `indexWhere` and / or `indexOf` of the
    * `Vector` class
    */
-  def findChar(c: Char, levelVector: Vector[Vector[Char]]): Pos = ???
+  def findChar(c: Char, levelVector: Vector[Vector[Char]]): Pos = {
+    val x = levelVector indexWhere( r => r contains c )
+    val y = levelVector(x).indexOf(c)
+    Pos(x,y)
+  }
 
   private lazy val vector: Vector[Vector[Char]] =
     Vector(level.split("\n").map(str => Vector(str: _*)): _*)
